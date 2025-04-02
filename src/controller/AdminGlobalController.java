7287e1122f85df6a381a16fc905e15c141bd2275
David Alakhume <98941567+Davalak@users.noreply.github.com>
2025-04-02 15:05:49 -0400

    Added navigation global bar for all admin pages

--- src/controller/AdminGlobalController.java
+++ src/controller/AdminGlobalController.java
@@ -3,16 +3,41 @@ package controller;
 import Model.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import javafx.scene.layout.Pane;
 import util.Navigation;
 
 public class AdminGlobalController {
 
     @FXML private Pane pagingPane;
+     @FXML private Label lblAdmin;
+    @FXML private Label lblDate;
+    @FXML private Label lblTime;
+
     private User currentUser;
 
     public void setUser(User user) {
         this.currentUser = user;
+
+        if (user != null) {
+            lblAdmin.setText(user.getFirstName() + " " + user.getLastName());
+        } else {
+            lblAdmin.setText("Admin");
+        }
+
+        setDateTime();
+    }
+
+    private void setDateTime() {
+        LocalDateTime now = LocalDateTime.now();
+
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("MMM dd, yyyy"); // e.g., Apr 02, 2025
+        DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("hh:mm a");       // e.g., 04:22 PM
+
+        lblDate.setText(now.format(dateFormat));
+        lblTime.setText(now.format(timeFormat));
     }
 
     @FXML
@@ -60,8 +85,8 @@ public class AdminGlobalController {
     }
 
     @FXML
-    private void btnBranchesOnAction(ActionEvent event) {
-        // TODO: Add support for branches page
+    private void btnLibrariansOnAction(ActionEvent event) {
+        // TODO: Add support for librarians page
     }
 
     @FXML
