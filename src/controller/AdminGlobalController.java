93778c2df5769ed5d22b2cc5d73a63a6bc87fec8
David Alakhume <98941567+Davalak@users.noreply.github.com>
2025-04-02 12:14:46 -0400

    created navigation bar form

---
+++ src/controller/AdminGlobalController.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+/**
+ * FXML Controller class
+ *
+ * @author ohigbai
+ */
+public class AdminGlobalController implements Initializable {
+
+    @FXML
+    private Pane catalogPane;
+    @FXML
+    private Label lblCatalog;
+    @FXML
+    private ImageView imgCatalog;
+    @FXML
+    private Pane dashboardPane;
+    @FXML
+    private Label lblDashboard;
+    @FXML
+    private ImageView imgDashboard;
+    @FXML
+    private Pane booksPane;
+    @FXML
+    private Label lblBooks;
+    @FXML
+    private ImageView imgBooks;
+    @FXML
+    private Pane usersPane;
+    @FXML
+    private Label lblUsers;
+    @FXML
+    private ImageView imgUsers;
+    @FXML
+    private Pane branchesPane;
+    @FXML
+    private Label lblBranches;
+    @FXML
+    private ImageView imgBranches;
+    @FXML
+    private Pane logOutPane;
+    @FXML
+    private Label lblLogOut;
+    @FXML
+    private ImageView imgLogOut;
+    @FXML
+    private Label lblAdmin;
+    @FXML
+    private Label lblTime;
+    @FXML
+    private Label lblDate;
+    @FXML
+    private Pane pagingPane;
+    @FXML
+    private ImageView imgTransparent;
+    @FXML
+    private Pane popUpPane;
+    @FXML
+    private Pane popUpLargePane;
+    @FXML
+    private Pane settingsPane;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
